91ea11025f3524037cb96fd03cb203d28be1c3a8
Salman Ahmed <say2salmanahmed@gmail.com>
2023-08-11 08:14:07 +0500

    Javascript Intro

---
+++ app.js
@@ -0,0 +1,25 @@
+var yourName = "Salman Ahmed";
+var yourAge = 21;
+console.log(yourName);
+console.log(yourAge);
+console.log("Your Name Is " + yourName + " & Your Age Is " + yourAge + ".");
+
+
+
+var num1 = 10;
+var num2 = 20;
+console.log(num1 / num2);
+console.log(num1 * num2);
+console.log(num1 + num2);
+console.log(num1 - num2);
+console.log(num1 , num2);
+
+
+
+var ok = "Sahi Hy";
+console.log(ok);
+
+
+
+var bodmasRule = (2+2*6-8) + 6*8 + (9*6/3*8) + 2/5 + (9-3) * 6 - 8 + 2;
+console.log(bodmasRule);
